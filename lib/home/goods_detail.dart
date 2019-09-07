6fc74152b6ed4984ed7e470afb971c0a3ccfea67
Monkey-Sun <492899051@qq.com>
2019-09-07 14:10:40 +0800

    提交代码，新增按钮倒计时

--- lib/home/goods_detail.dart
+++ lib/home/goods_detail.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/animation_navigator.dart';
+
+const int COUNTTIME = 20;
 
 class GoodsDetailPage extends StatefulWidget {
-  final String productDesc; //final 只能被赋值一次
+  final String productDesc;
   GoodsDetailPage({this.productDesc, int option}); // [type varname] 可选参数
   @override
   State<StatefulWidget> createState() {
@@ -12,31 +15,52 @@ class GoodsDetailPage extends StatefulWidget {
 }
 
 class GoodsDetailState extends State<GoodsDetailPage> {
-  String desc;
-
   GoodsDetailState(this.desc);
-
+  String desc;
+  String buttonTitle = "获取验证码";
+  int  second = COUNTTIME;
+  Timer countTimer;
+  bool buttonEnable = true;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    VoidCallback onPressHandle = () {
+      String tempTitle;
+      countTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+        second -= 1;
+        if (this.second <= 1) {
+          tempTitle = "获取验证码";
+          second = COUNTTIME;
+          timer.cancel();
+        } else {
+          tempTitle = second.toString() + 's后重新获取';
+        }
+        setState(() {
+          buttonTitle = tempTitle;
+          buttonEnable = second == COUNTTIME;
+        });
+      });
+    };
+
+    FlatButton button = new FlatButton(onPressed: (buttonEnable ? onPressHandle : null), textColor: Colors.red,child: new Text(buttonTitle), disabledColor: Colors.grey,);
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("商祥"),
-        ),
-        body: new Container(
-          padding: EdgeInsets.all(0),
-          child: new FlatButton(
-            onPressed: () {
-              AnimationNavi.pop(context, "我日你先人板凳");
-            },
-            child: new Text(desc),
-          ),
-        ));
+      appBar: new AppBar(
+        title: new Text("商详"),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(0),
+        child: button,
+      ),
+    );
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
+    if(countTimer != null){
+      countTimer.cancel();
+    }
   }
 }
